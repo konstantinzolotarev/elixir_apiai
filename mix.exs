6aa38f004da809ab77657008090809e28b759834
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2017-08-16 17:02:54 +0300

    Fix mix.exs

--- mix.exs
+++ mix.exs
@@ -1,28 +1,37 @@
 defmodule ElixirApiai.Mixfile do
   use Mix.Project
 
+  @version File.read!("VERSION") |> String.trim
+
   def project do
     [
       app: :elixir_apiai,
-      version: "0.1.0",
-      elixir: "~> 1.5",
-      start_permanent: Mix.env == :prod,
-      deps: deps()
+      version: @version,
+      elixir: "~> 1.4",
+      docs: [ extras: ["README.md"] ],
+      description: "Elixir Api.ai SDK client",
+      deps: deps(),
+      package: package()
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger, :httpotion]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:poison, "~> 3.1.0"},
       {:httpotion, "~> 3.0.3"}
     ]
+  end
+
+  defp package do
+    [ files: [ "lib", "mix.exs", "README.md", "VERSION", "LICENSE" ],
+      maintainers: [ "Konstantin Zolotarev" ],
+      licenses: [ "MIT" ],
+      links: %{ "GitHub" => "https://github.com/konstantinzolotarev/elixir_apiai" } ]
   end
 end
